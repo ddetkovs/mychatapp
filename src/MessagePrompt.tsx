9c3271ffc7450378294de6fdc28d24dc5c88e022
Deniss Detkovs <DenissDetkovs@fico.com>
2022-03-17 22:02:50 +0100

    message prompt styling

--- src/MessagePrompt.tsx
+++ src/MessagePrompt.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './MessagePrompt.module.scss';
 export const MessagePrompt = () => {
   return (
-    <div className={styles['message-prompt-container']}>
+    <div className={styles['message-prompt-bar-container']}>
       <form>
         <input type="text" />
         <button>Send</button>
