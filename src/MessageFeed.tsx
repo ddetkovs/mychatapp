b88187fb094f9c88653549f7e4ecec752c51dbcb
Deniss Detkovs <DenissDetkovs@fico.com>
2022-03-18 00:42:21 +0100

    styling when sending message is in progress

--- src/MessageFeed.tsx
+++ src/MessageFeed.tsx
@@ -2,9 +2,7 @@ import classNames from 'classnames';
 import React, { useContext } from 'react';
 import { UserContext } from './App';
 import styles from './MessageFeed.module.scss';
-import { Message, Messages } from './useMessages';
-
-
+import { Message, Messages, SentMessage } from './useMessages';
 
 const dateTimeFormatter = Intl.DateTimeFormat('default', {
   year: 'numeric',
@@ -15,12 +13,19 @@ const dateTimeFormatter = Intl.DateTimeFormat('default', {
 });
 const formatTimestamp = (timestamp: number) => dateTimeFormatter.format(new Date(timestamp));
 
+const isMessageSending = (message: Message): message is SentMessage => {
+  return (message as SentMessage).sending === true;
+}
+
 const MessageEntry = ({ message }: { message: Message }) => {
   const userName = useContext(UserContext);
   const isSelf = message.author === userName;
+  const isSending = isMessageSending(message);
 
   return (
-    <div className={classNames(styles['message-entry'], { [styles['own-message']]: isSelf })}>
+    <div
+      className={classNames(styles['message-entry'], { [styles['own-message']]: isSelf, [styles.sending]: isSending })}
+    >
       {!isSelf && <p className={styles['message-details']}>{message.author}</p>}
       <p className={styles['message-body']}>{message.message}</p>
       <p className={styles['message-details']}>{formatTimestamp(message.timestamp)}</p>
