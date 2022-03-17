7397ec5487b2281917c58b275b6cd1eabd381d42
Deniss Detkovs <DenissDetkovs@fico.com>
2022-03-17 22:25:05 +0100

    dummy messages and message styling

--- src/MessageFeed.tsx
+++ src/MessageFeed.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import styles from './MessageFeed.module.scss';
-const Message = ({}) => {
+
+type Message = {
+  _id: string;
+  message: string;
+  author: string;
+  timestamp: number;
+};
+
+type Messages = Message[];
+
+const messages: Messages = [
+  {
+    _id: '61b046a648c220001b5f6c7f',
+    message: 'Hello world',
+    author: 'Tom',
+    timestamp: 1638942374690,
+  },
+  {
+    _id: '61b04b3148c220001b5f6c80',
+    message: 'Test message',
+    author: 'RandomUser',
+    timestamp: 1638943537253,
+  },
+  {
+    _id: '61b05ae748c220001b5f6c81',
+    message: 'Messages are tested',
+    author: 'Me',
+    timestamp: 1638947559815,
+  },
+];
+
+const MessageEntry = ({ message }: { message: Message }) => {
   return (
-    <div>
-      <p>author</p>
-      <p>message</p>
-      <p>details</p>
+    <div className={styles['message-entry']}>
+      <p>{message.author}</p>
+      <p>{message.message}</p>
+      <p>{message.timestamp}</p>
     </div>
   );
 };
@@ -13,10 +44,9 @@ const Message = ({}) => {
 export const MessageFeed = () => {
   return (
     <div className={styles['message-feed-container']}>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
+      {messages.map((message) => (
+        <MessageEntry key={message._id} message={message}></MessageEntry>
+      ))}
     </div>
   );
 };
