3ee2c2e1b59dff1efc4c2f6e2e8ed34a32f24a70
Deniss Detkovs <DenissDetkovs@fico.com>
2022-03-17 23:02:20 +0100

    clear input when submitting new message

--- src/MessagePrompt.tsx
+++ src/MessagePrompt.tsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import styles from './MessagePrompt.module.scss';
 export const MessagePrompt = ({ sendMessage }: { sendMessage: (message: string) => void }) => {
   const [newMessage, setNewMessage] = useState('');
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    setNewMessage('');
+    sendMessage(newMessage);
+    event.preventDefault();
+  };
   return (
     <div className={styles['message-prompt-bar-container']}>
-      <form onSubmit={() => sendMessage(newMessage)}>
-        <input type="text" onChange={(event) => setNewMessage(event.target.value)} />
+      <form onSubmit={onFormSubmit}>
+        <input
+          placeholder="Type a message"
+          type="text"
+          value={newMessage}
+          onChange={(event) => setNewMessage(event.target.value)}
+        />
         <button disabled={newMessage.trim().length === 0}>Send</button>
       </form>
     </div>
